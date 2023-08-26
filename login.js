d854db21be5413804e90704035d4f50422d87452
TheGreatMaximus98 <maximusjdhalloran@gmail.com>
2023-08-26 10:34:55 -0400

    upadte

---
+++ login.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuYXfAsSUQP-1gNRXXDetCU-KDdPDnpwY",
+  authDomain: "socialcontent-db.firebaseapp.com",
+  projectId: "socialcontent-db",
+  storageBucket: "socialcontent-db.appspot.com",
+  messagingSenderId: "488254523586",
+  appId: "1:488254523586:web:071e852334555ebcbb135b",
+  measurementId: "G-HYD48R46J9"
+};
+const app = initializeApp(firebaseConfig);
